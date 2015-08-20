2a43c051c8f76ed07c53e6d8ca220a57380b78d8
Caleb Francis <calebonesimus@gmail.com>
2015-08-20 14:56:29 -0400

    Adds shoulda gem and creates some item tests

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :name, :price, :category, :desription, :user_id
+  validates_presence_of :name, :price, :category, :description, :user_id
 
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -1,7 +1,47 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  should belong_to :user
+
+  test "item must have name" do
+    item = Item.new(price: "200000",
+                    description: "Apple laptop",
+                    category: "electronics",
+                    user_id: 1)
+    assert_not item.save
+  end
+
+  test "item must have price" do
+    item = Item.new(name: "MacBook",
+                    description: "Apple laptop",
+                    category: "electronics",
+                    user_id: 1)
+    assert_not item.save
+  end
+
+  test "item must have description" do
+    item = Item.new(name: "MacBook",
+                    price: "200000",
+                    category: "electronics",
+                    user_id: 1)
+    assert_not item.save
+  end
+
+  test "item must have category" do
+    item = Item.new(name: "MacBook",
+                    price: "200000",
+                    description: "Apple laptop",
+                    user_id: 1)
+    assert_not item.save
+  end
+
+  test "item must belong to a user" do
+    item = Item.new(name: "MacBook",
+                    price: "200000",
+                    description: "Apple laptop",
+                    category: "electronics",)
+    assert_not item.save
+  end
+
 end
