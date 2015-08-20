542f4e7aec68bc4b2e81f82131bc9e0288e898c8
Caleb Francis <calebonesimus@gmail.com>
2015-08-20 14:42:17 -0400

    Adds item validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :name, :price, :category, :desription
-  
+  validates_presence_of :name, :price, :category, :desription, :user_id
+
 end
