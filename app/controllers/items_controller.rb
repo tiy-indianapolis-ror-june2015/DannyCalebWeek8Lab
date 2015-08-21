a47da9fb5de0663c6acfcdc6ae4c1aa607773a26
Caleb Francis <calebonesimus@gmail.com>
2015-08-21 12:35:36 -0400

    Create working cart

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,18 @@
 class ItemsController < ApplicationController
+
   def index
     @items = Item.all
+    if session[:cart_id].nil?
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
   end
+
+  def add_to_cart
+    item = Item.find(params[:item_id])
+    cart = Cart.find(session[:cart_id])
+    cart.items << item
+    redirect_to :back
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820183246) do
+ActiveRecord::Schema.define(version: 20150821162909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "category"
@@ -24,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150820183246) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "cart_id"
   end
 
   create_table "payola_affiliates", force: :cascade do |t|
