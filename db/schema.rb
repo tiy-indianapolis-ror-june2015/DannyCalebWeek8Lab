2a4038930855b7d22c41e7f9646169e989b81419
Daniel Beyrer <beyrerdn@gmail.com>
2015-08-21 10:57:41 -0400

    User is able to view items and redirect back to the root page. Permalinks added but not fully working.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820183246) do
+ActiveRecord::Schema.define(version: 20150821144141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,10 @@ ActiveRecord::Schema.define(version: 20150820183246) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "permalink"
   end
+
+  add_index "items", ["permalink"], name: "index_items_on_permalink", using: :btree
 
   create_table "payola_affiliates", force: :cascade do |t|
     t.string   "code"
