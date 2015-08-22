11620e23592c408df27a5b9aef6156e0c5b49823
Daniel Beyrer <beyrerdn@gmail.com>
2015-08-22 16:02:01 -0400

    Adding an item to the cart that is already in the cart just updates quantity. Carts can have a user, but for some reason users can't have a cart. The active session cart user_id is set equal to current_user.id after a user is created. When user logs out, the visitor sees a new cart, not the one the logged-in customer was viewing.

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,33 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  after_filter :set_customer_cart, :only => :create
+
+  def create
+    super
+  end
+
+  private
+
+  def set_customer_cart
+    Cart.find(session[:cart_id]).update_attributes(:user_id => current_user.id)
+  end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.for(:sign_up) << :attribute
+  # end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.for(:account_update) << :attribute
+  # end
+
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+end
