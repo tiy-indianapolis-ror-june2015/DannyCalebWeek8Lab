17d8089006179d96509291631937b65415410f89
Caleb Francis <calebonesimus@gmail.com>
2015-08-24 16:12:29 -0400

    Adds methods for sending mailer

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -18,30 +18,30 @@ class ChargesController < ApplicationController
       :currency    => 'usd'
     )
 
-    create_receipt
+    create_receipt(customer)
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to charges_path
-
   end
-
 
   private
   # Create a receipt for the user
+  # Email the receipt
   # Clear the cart
   # Show the receipt
-  def create_receipt
+  def create_receipt(customer)
     cart = Cart.find(session[:cart_id])
     if current_user
       receipt = Receipt.create(user_id: current_user.id)
       receipt.purchased_items = receipt.purchased_items + prepare_receipt_items(current_user.cart.items)
       receipt.update_attributes(checkout_total: cookies[:checkout_total])
-      UserReceipt.send_receipt_email(current_user, receipt).deliver
+      UserReceipt.send_receipt_email(customer, receipt).deliver_now
     else
       receipt = Receipt.create
       receipt.purchased_items = receipt.purchased_items + prepare_receipt_items(Cart.find(session[:cart_id]).items)
       receipt.update_attributes(checkout_total: cookies[:checkout_total])
+      UserReceipt.send_receipt_email(customer, receipt).deliver_now
     end
     clear_cart(cart)
     redirect_to show_receipt_path(receipt_id: receipt.id)
